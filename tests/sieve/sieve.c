1520abfd692e6f024ac90c056329105c5e627d73
soniloi <niloinso@tcd.ie>
2015-01-29 13:09:32 +0000

    Added check to sieve test; fixed a small bug

--- tests/sieve/sieve.c
+++ tests/sieve/sieve.c
@@ -3,10 +3,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define LEN_CORRECT 128
+
 typedef struct timespec timespec;
 
-int * sieve(int n)
-{
+const int correct[LEN_CORRECT] = {0, 0, 1, 1, 0, 1, 0, 1, // 0-7
+                                  0, 0, 0, 1, 0, 1, 0, 0, // 8-15
+                                  0, 1, 0, 1, 0, 0, 0, 1, // 16-23
+                                  0, 0, 0, 0, 0, 1, 0, 1, // 24-31
+                                  0, 0, 0, 0, 0, 1, 0, 0, // 32-39
+                                  0, 1, 0, 1, 0, 0, 0, 1, // 40-47
+                                  0, 0, 0, 0, 0, 1, 0, 0, // 48-55
+                                  0, 0, 0, 1, 0, 1, 0, 0, // 56-63
+                                  0, 0, 0, 1, 0, 0, 0, 1, // 64-71
+                                  0, 1, 0, 0, 0, 0, 0, 1, // 72-79
+                                  0, 0, 0, 1, 0, 0, 0, 0, // 80-87
+                                  0, 1, 0, 0, 0, 0, 0, 0, // 88-95
+                                  0, 1, 0, 0, 0, 1, 0, 1, // 96-103
+                                  0, 0, 0, 1, 0, 1, 0, 0, // 104-111
+                                  0, 1, 0, 0, 0, 0, 0, 0, // 112-119
+                                  0, 0, 0, 0, 0, 0, 0, 1}; // 120-127
+
+int * sieve(int n) {
 	int * isprime;
 	isprime = malloc(sizeof(int) * n);
 
@@ -18,8 +36,8 @@ int * sieve(int n)
 	isprime[0] = 0;
 	isprime[1] = 0;
 
-	int bound = sqrt(n);
-	for(i = 2; i < bound; i++)
+	int bound = sqrt(n) + 1; // Allow for possible FP rounding errors
+	for(i = 2; i <= bound; i++)
 	{
 		if(isprime[i])
 		{
@@ -95,6 +113,17 @@ int main(int argc, char ** argv)
 			total_time += elapsed;
 
 		//printf("Iteration %d: time elapsed: %llu microseconds\n", i+1, elapsed);
+
+		// Test output for correctness
+		int max = LEN_CORRECT;
+		if(size < max)
+			max = size;
+		int j;
+		for(j = 0; j < max; j++)
+		{
+			if(isprime[j] != correct[j])
+				printf("Error on iteration %d: program calculates %d=%d, correct is %d=%d\n", i, j, isprime[j], j, correct[j]);
+		}
 
 		free(isprime);
 	}
