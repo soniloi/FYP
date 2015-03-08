6d8cfc6e1fcd652b869f0381a3c547eb94518d8d
soniloi <niloinso@tcd.ie>
2015-03-08 13:39:42 +0000

    Tidied up concat script

--- overflow/amalgam/concat.py
+++ overflow/amalgam/concat.py
@@ -13,85 +13,93 @@ funcdir = 'funcs'
 fileoutpath = 'humpty.c'
 sep = '/*****************************************************************************/\n'
 
-# Generate list of prototypes
-subprocess.call([protogen, protogenopt, original, '-o', prototypespath])
+# Output formatted header to file
+def output_header(fileout, label):
+	fileout.write('\n' + sep + '/** ' + label + ' **/\n' + sep + '\n')
 
-# Generate list of function filenames
-funcarr = os.listdir(funcdir)
-funcarr.sort()
-#print funcarr
+# Generate array of indices in simple in-order sequence
+def generate_identity_permutation(arrlen):
+	indarr = []
+	for i in range(0, arrlen):
+		indarr.append(i)
+	return indarr
 
-indarr = []
-arrlen = len(funcarr)
+def main():	
+	# Generate list of prototypes
+	subprocess.call([protogen, protogenopt, original, '-o', prototypespath])
 
-# Simple in-order sequence 
-for i in range(0, arrlen):
-	indarr.append(i)
+	# Generate list of function filenames
+	funcarr = os.listdir(funcdir)
+	funcarr.sort()
+	#print funcarr
 
-'''
-# Reverse-order sequence
-for i in range(0, arrlen-1):
-	indarr.append(arrlen-i-2)
-'''
+	arrlen = len(funcarr)
+	indarr = generate_identity_permutation(arrlen)
 
-'''
-# Simple interleaving
-halfway = arrlen/2
-for i in range(0, halfway):
+	'''
+	# Simple in-order sequence 
+	for i in range(0, arrlen):
+		indarr.append(i)
+	'''
+
+	'''
+	# Reverse-order sequence
+	for i in range(0, arrlen-1):
+		indarr.append(arrlen-i-2)
+	'''
+
+	'''
+	# Simple interleaving
+	halfway = arrlen/2
+	for i in range(0, halfway):
 	indarr.append(i)
 	if i*2 < arrlen: # FIXME: might have to add 1 here
 		indarr.append(i+halfway)
-'''
+	'''
 
-#print indarr
+	#print indarr
 
-# Erase artefacts of previous runs
-if os.path.isfile(fileoutpath):
-	os.remove(fileoutpath)
+	# Erase artefacts of previous runs
+	if os.path.isfile(fileoutpath):
+		os.remove(fileoutpath)
 
-fileout = open(fileoutpath, 'w+')
+	fileout = open(fileoutpath, 'w+')
 
-# Generate file; output 'header'
-fileout.write('\n' + sep)
-fileout.write('/** Global Declarations **/\n')
-fileout.write(sep + '\n')
-with open(headerpath, 'r') as headerfile:
-	for line in headerfile:
-		fileout.write(line)
-
-# Concatenate forward declarations
-fileout.write('\n' + sep)
-fileout.write('/** Forward Declarations **/\n')
-fileout.write(sep + '\n')
-with open(prototypespath, 'r') as prototypesfile:
-	for line in prototypesfile:
-		fileout.write(line)
-
-# Concatenate macros in original order
-fileout.write('\n' + sep)
-fileout.write('/** Macros **/\n')
-fileout.write(sep + '\n')
-macropaths = os.listdir(macrodir)
-macropaths.sort()
-for macrofilepath in macropaths:
-	with open(macrodir + '/' + macrofilepath, 'r') as macrofile:
-		for line in macrofile:
+	# Generate file; output 'header'
+	output_header(fileout, 'Global Declarations')
+	with open(headerpath, 'r') as headerfile:
+		for line in headerfile:
 			fileout.write(line)
+
+	# Concatenate forward declarations
+	output_header(fileout, 'Forward Declarations')
+	with open(prototypespath, 'r') as prototypesfile:
+		for line in prototypesfile:
+			fileout.write(line)
+
+	# Concatenate macros in original order
+	output_header(fileout, 'Macros')
+	macropaths = os.listdir(macrodir)
+	macropaths.sort()
+	for macrofilepath in macropaths:
+		with open(macrodir + '/' + macrofilepath, 'r') as macrofile:
+			for line in macrofile:
+				fileout.write(line)
+			fileout.write('\n\n')
+
+	# Concatenate functions in specified order
+	output_header(fileout, 'Functions')
+	funcpaths = os.listdir(funcdir)
+	funcpaths.sort()
+	for index in indarr:
+		print str(index) + '\t',
+		if index%20 == 0:
+			print
+		with open(funcdir + '/' + funcpaths[index], 'r') as funcfile:
+			for line in funcfile:
+				fileout.write(line)
 		fileout.write('\n\n')
 
-# Concatenate functions in specified order
-fileout.write('\n' + sep)
-fileout.write('/** Functions **/\n')
-fileout.write(sep + '\n')
-funcpaths = os.listdir(funcdir)
-funcpaths.sort()
-for index in indarr:
-	print str(index) + '\t',
-	if index%20 == 0:
-		print
-	with open(funcdir + '/' + funcpaths[index], 'r') as funcfile:
-		for line in funcfile:
-			fileout.write(line)
-		fileout.write('\n\n')
+	fileout.close()
 
-fileout.close()
+main()
