6c1489961024756a811f7f6a78957ebdbb676f43
soniloi <niloinso@tcd.ie>
2015-03-09 09:53:06 +0000

    Concat script generates random permutations

--- overflow/amalgam/concat.py
+++ overflow/amalgam/concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 
 import os
+import random
 import subprocess
 
 original = 'amalgamation.c'
@@ -24,7 +25,22 @@ def generate_identity_permutation(arrlen):
 		indarr.append(i)
 	return indarr
 
-def main():	
+# Generate a random permutation of a given input array
+def generate_random_permutation(arr):
+	permutation = list(arr)
+	arrlen = len(permutation)
+	for i in range(0, arrlen):
+		j = random.randint(i, arrlen-1)
+		#print "arrlen: " + str(arrlen) +  " i: " + str(i)  + " j: " + str(j)
+		temp = permutation[i]
+		permutation[i] = permutation[j]
+		permutation[j] = temp
+	return permutation
+
+def main():
+	# Seed random number generator for choosing order of permutations
+	random.seed(13)
+
 	# Generate list of prototypes
 	subprocess.call([protogen, protogenopt, original, '-o', prototypespath])
 
@@ -34,30 +50,8 @@ def main():
 	#print funcarr
 
 	arrlen = len(funcarr)
-	indarr = generate_identity_permutation(arrlen)
-
-	'''
-	# Simple in-order sequence 
-	for i in range(0, arrlen):
-		indarr.append(i)
-	'''
-
-	'''
-	# Reverse-order sequence
-	for i in range(0, arrlen-1):
-		indarr.append(arrlen-i-2)
-	'''
-
-	'''
-	# Simple interleaving
-	halfway = arrlen/2
-	for i in range(0, halfway):
-	indarr.append(i)
-	if i*2 < arrlen: # FIXME: might have to add 1 here
-		indarr.append(i+halfway)
-	'''
-
-	#print indarr
+	identityarr = generate_identity_permutation(arrlen)
+	indarr = generate_random_permutation(identityarr)
 
 	# Erase artefacts of previous runs
 	if os.path.isfile(fileoutpath):
