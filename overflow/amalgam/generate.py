bc81af6ae5bad38ba68b968030a30e5064db1af2
soniloi <niloinso@tcd.ie>
2015-03-10 15:16:40 +0000

    Removed a load of commented-out junk

--- overflow/amalgam/generate.py
+++ overflow/amalgam/generate.py
@@ -14,11 +14,6 @@ prototypespath = 'prototypes.lst'
 macrodir = 'first'
 funcdir = 'funcs'
 fileoutpath = 'humpty.c'
-#sep = '/*****************************************************************************/\n'
-
-# Output formatted header to file
-def output_header(fileout, label):
-	fileout.write('\n' + sep + '/** ' + label + ' **/\n' + sep + '\n')
 
 # Generate array of indices in simple in-order sequence
 def generate_identity_permutation(arrlen):
@@ -33,13 +28,12 @@ def generate_random_permutation(arr):
 	arrlen = len(permutation)
 	for i in range(0, arrlen):
 		j = random.randint(i, arrlen-1)
-		#print "arrlen: " + str(arrlen) +  " i: " + str(i)  + " j: " + str(j)
 		temp = permutation[i]
 		permutation[i] = permutation[j]
 		permutation[j] = temp
 	return permutation
 
-def main():
+def generate():
 	# Seed random number generator for choosing order of permutations
 	random.seed(13)
 
@@ -49,8 +43,8 @@ def main():
 	# Generate list of function filenames
 	funcarr = os.listdir(funcdir)
 	funcarr.sort()
-	#print funcarr
 
+	# Get permutation order
 	arrlen = len(funcarr)
 	identityarr = generate_identity_permutation(arrlen)
 	indarr = generate_random_permutation(identityarr)
@@ -62,45 +56,4 @@ def main():
 	# Concatenate file
 	concat.concat_source(fileoutpath, headerpath, prototypespath, macrodir, funcdir, indarr)
 
-	'''
-	fileout = open(fileoutpath, 'w+')
-
-	# Generate file; output 'header'
-	output_header(fileout, 'Global Declarations')
-	with open(headerpath, 'r') as headerfile:
-		for line in headerfile:
-			fileout.write(line)
-
-	# Concatenate forward declarations
-	output_header(fileout, 'Forward Declarations')
-	with open(prototypespath, 'r') as prototypesfile:
-		for line in prototypesfile:
-			fileout.write(line)
-
-	# Concatenate macros in original order
-	output_header(fileout, 'Macros')
-	macropaths = os.listdir(macrodir)
-	macropaths.sort()
-	for macrofilepath in macropaths:
-		with open(macrodir + '/' + macrofilepath, 'r') as macrofile:
-			for line in macrofile:
-				fileout.write(line)
-			fileout.write('\n\n')
-
-	# Concatenate functions in specified order
-	output_header(fileout, 'Functions')
-	funcpaths = os.listdir(funcdir)
-	funcpaths.sort()
-	for index in indarr:
-		print str(index) + '\t',
-		if index%20 == 0:
-			print
-		with open(funcdir + '/' + funcpaths[index], 'r') as funcfile:
-			for line in funcfile:
-				fileout.write(line)
-		fileout.write('\n\n')
-
-	fileout.close()
-	'''
-
-main()
+generate()
