b7312c3e97d65302c675b5b395a5deb5f510c881
soniloi <niloinso@tcd.ie>
2015-03-11 10:33:25 +0000

    Added script to check whether stack smashed

---
+++ overflow/amalgam/smashcheck.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Check whether running a binary with specific given payloads overflows the call stack
+### Program does nothing useful; it is just started and then exits; it has been
+###  arranged so that the stack will be smashed on exit
+### If no overflow occurs (or if the attempt causes a segmentation fault), then 
+###  there will be no output
+
+usage="Usage: $0 <path-to-binary>"
+grepnc="grep --color=never"
+pattern_smash="process [0-9][0-9]* is executing new program: /bin/bash"
+
+if [[ "$#" -ne 1 ]]; then
+	echo $usage
+	exit 1
+fi
+
+bin=$1
+if ! [[ -x $bin ]]; then
+	echo $usage
+	exit 1
+fi
+
+logfile="run.log"
+
+gdb $bin -ex run > $logfile 2>&1 &
+wait
+$grepnc "$pattern_smash" $logfile
+
+# Clean up
+rm -f $logfile
