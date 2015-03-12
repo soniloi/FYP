7bd3e975b889ee8bb4de747f001a0454b4d4f133
soniloi <niloinso@tcd.ie>
2015-03-12 12:18:50 +0000

    Added script to check binary size

---
+++ overflow/amalgam/sizecheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### Return the size of a file in bytes
+
+usage="Usage: $0 <path-to-binary>"
+sizer="du -b"
+
+if [[ "$#" -ne 1 ]]; then
+	echo $usage
+	exit 1
+fi
+
+bin=$1
+if ! [[ -f $bin ]]; then
+	echo $usage
+	exit 1
+fi
+
+sizebinline=`$sizer $bin`
+sizebin=($sizebinline)
+echo $sizebin
