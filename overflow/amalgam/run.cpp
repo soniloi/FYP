0b6466147d7268e1d8221ab9479cf6b9abc4fdd8
soniloi <niloinso@tcd.ie>
2015-03-17 11:34:46 +0000

    Added function to retrieve list of filenames in directory

--- overflow/amalgam/run.cpp
+++ overflow/amalgam/run.cpp
@@ -1,4 +1,5 @@
-#include <algorithm> // shuffle
+#include <algorithm> // shuffle, sort
+#include <dirent.h> // directory operations
 #include <iostream>
 #include <cstdlib>
 #include <random>
@@ -9,9 +10,15 @@ using namespace std;
 
 const string usage = "run <path-to-build-tree> <total-generations> <initial-seed>";
 const string original = "./amalgamation.c";
+const string headerpath = "./header.c";
+const string macropath = "./macros.c";
+const string funcdir = "./funcs";
 const string prototypespath = "./prototypes.lst";
 const string protogen = "cproto";
 const string protogenopt = "-si";
+
+const string binname = "./humpty";
+//const string fileoutpath = 
 
 vector<int> generate_random_permutation(vector<int> original, std::mt19937 rng){
 	vector<int> result(original);
@@ -25,6 +32,29 @@ void generate_prototypes(){
 	stringstream protocom;
 	protocom << protogen << ' ' << protogenopt << ' ' <<  "-o " << prototypespath << ' ' << original;
 	system(protocom.str().c_str());
+}
+
+// Get sorted relative list of filenames from a directory
+vector<string> get_filenames(string dirpath){
+	vector<string> filearr;
+
+	DIR * dir;
+	struct dirent * ent;
+
+	dir = opendir(dirpath.c_str());
+	if(dir){
+		while(ent = readdir(dir)){
+			string dname = ent->d_name;
+			if(dname.compare(".") && dname.compare("..")){
+				stringstream filepath;
+				filepath << dirpath << '/' << dname;
+				filearr.push_back(filepath.str());
+			}
+		}
+	}
+
+	std::sort(filearr.begin(), filearr.end());
+	return filearr;
 }
 
 // Create array for which every value is the same as its index
@@ -46,20 +76,27 @@ int main(int argc, char ** argv){
 	int generation_count = atoi(argv[2]); // Number of permutations of source to generate
 	int initial_seed = atoi(argv[3]);
 
-	cout << "daa: " << daa << " generation_count: " << generation_count << " initial_seed: " << initial_seed << endl;
-
+	// Initialize RNG
 	std::mt19937 rng;
 	std::uniform_int_distribution<uint32_t> dist(0, 16777215);
 	rng.seed(initial_seed);
 
-	vector<int> identity = identity_array(1800);
+	// Generate common file listing function prototypes
+	generate_prototypes();
+
+	// Retrieve sorted list of function filenames
+	vector<string> funcarr = get_filenames(funcdir);
+	for(vector<string>::iterator it = funcarr.begin(); it != funcarr.end(); it++){
+		cout << (*it) << endl;
+	}
+	cout << "Total number of files: " << funcarr.size() << endl;
+
+	vector<int> identity = identity_array(funcarr.size());
 	
 	vector<int> index_array = generate_random_permutation(identity, rng);
 	for(int i = 0; i < index_array.size(); i++){
 		cout << i << ": " << index_array[i] << endl;
 	}
-
-		generate_prototypes(); // Generate the common file listing function prototypes
 
 	
 
