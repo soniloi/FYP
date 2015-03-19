93f58f463b2df8a2a812c8ef8aee4eb8a8971ffd
soniloi <niloinso@tcd.ie>
2015-03-19 11:34:10 +0000

    Added script to put out-of-order logs back in order

---
+++ overflow/amalgam/reform-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### Take a logfile that might be written to in any order and re-write it in order
+
+usage="$0 <path-to-input-logfile> <number-of-versions> <path-to-output-logfile>"
+vprefix='version-'
+spattern='[summary]'
+grepnc='grep --color=never'
+
+if [[ "$#" -ne 3 ]]; then
+	echo $usage
+	exit 1
+fi
+
+logfilein=$1
+if ! [[ -f $logfilein ]]; then
+	echo $usage
+	exit 1
+fi
+
+versions=$2
+logfileout=$3
+
+rm -f $logfileout
+touch $logfileout
+
+for i in `seq 1 $versions`; do
+	echo $i
+	label="$vprefix$i"
+	$grepnc "\<$label\>" $logfilein >> $logfileout
+	echo -e "\n\n" >> $logfileout
+done
+
+$grepnc $spattern $logfilein >> $logfileout
