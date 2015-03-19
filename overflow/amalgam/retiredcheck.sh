c451f31c99a1e91a098a9430a89384b4f3314deb
soniloi <niloinso@tcd.ie>
2015-03-19 21:10:28 +0000

    Changed retiredcheck to run the program without the datafiles available

--- overflow/amalgam/retiredcheck.sh
+++ overflow/amalgam/retiredcheck.sh
@@ -21,6 +21,20 @@ fi
 
 bindir=`dirname $bin`
 temp="$bindir/rettempp"
+data1="$bindir/data1.dat"
+data2="$bindir/data2.dat"
+data1saved="$data1.saved"
+data2saved="$data2.saved"
+
+# We need the datafiles not to be there; segmentation fault of itself causes a difference in retired instructions
+if [[ -f "$data2" ]]; then
+	mv $data2 $data2saved
+fi
+
+if [[ -f "$data1" ]]; then
+	mv $data1 $data1saved
+fi
+
 
 cat $input | $counter $stat $bin > $temp 2>&1
 retline=`$grepnc "$stat" $temp`
@@ -28,7 +42,16 @@ retline=`$grepnc "$stat" $temp`
 retwords=($retline)
 retinsts=`echo ${retwords[0]} | tr -d ,`
 
+# Clean up
 rm -f $temp
+
+if [[ -f "$data1saved" ]]; then
+	mv $data1saved $data1
+fi
+
+if [[ -f "$data2saved" ]]; then
+	mv $data2saved $data2
+fi
 
 echo -n $retinsts
 
