f4815c31253c4cdd418510867cb1989fdf29db38
soniloi <niloinso@tcd.ie>
2015-03-29 04:48:24 +0100

    FuncGenerator generating functions recursively at random with seed passed

--- autogen/FuncGenerator.py
+++ autogen/FuncGenerator.py
@@ -2,7 +2,9 @@
 import random
 
 max_statements = 20 # Maximum number of statements in a basic block
-operators = ['+', '-', '*']
+new_block_chance = 20 # One in this many statements, a new basic block will be opened
+operators_arith = ['+', '-', '*']
+operators_bool = ['==', '!=', '<', '>', '<=', '>=']
 operands = ['z', 'y', 'x']
 
 def get_spacing(depth):
@@ -11,18 +13,37 @@ def get_spacing(depth):
 		spacing += '\t'
 	return spacing
 
-def get_random_statement():
-	stat = ''
+def get_closing(depth):
+	clo = get_spacing(depth)
+	clo += '}'
+	return clo
+
+def get_statement(depth):
+	stat = get_spacing(depth)
 	stat += operands[random.randint(0, len(operands)-1)] + ' = '
 	stat += operands[random.randint(0, len(operands)-1)] + ' '
-	stat += operators[random.randint(0, len(operators)-1)] + ' '
+	stat += operators_arith[random.randint(0, len(operators_arith)-1)] + ' '
 	stat += operands[random.randint(0, len(operands)-1)] + ';'
 	return stat
 
+def get_condition(depth):
+	cond = get_spacing(depth)
+	cond += ('if(')
+	cond += operands[random.randint(0, len(operands)-1)] + ' '
+	cond += operators_bool[random.randint(0, len(operators_bool)-1)] + ' '
+	cond += operands[random.randint(0, len(operands)-1)] + '){'
+	return cond
+
 def get_basic_block(statements_sofar, depth):
 	for i in range(0, random.randint(0, max_statements)):
-		statement = get_spacing(depth) + get_random_statement()
+		statement = get_statement(depth)
 		statements_sofar.append(statement)
+		if random.randint(0, new_block_chance-1) == 0:
+			statements_sofar.append('')
+			statements_sofar.append(get_condition(depth))
+			get_basic_block(statements_sofar, depth + 1)
+			statements_sofar.append(get_closing(depth))
+			statements_sofar.append('')
 
 def generate_function(funcname, seed):
 	random.seed(seed)
@@ -32,6 +53,7 @@ def generate_function(funcname, seed):
 
 	get_basic_block(lines, 1)
 
-	lines.append('}')
+	lines.append('\tprintf("%d", z);')
+	lines.append(get_closing(0))
 
 	return lines
