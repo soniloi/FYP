0ad1e5229b930f8d5a06a9e2eb5602b8fce7c3f6
soniloi <niloinso@tcd.ie>
2015-03-31 11:54:05 +0100

    Differentiated smashing user copy and smashing root copy

---
+++ smash-demo/user-copy/smash-root.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Smash root copy of the eggshell
+
+set -x
+
+whoami
+../root-copy/eggshell data1.dat data2.dat
--- smash-demo/user-copy/smash-user.sh
+++ smash-demo/user-copy/smash-user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-## Smash the eggshell
+## Smash user copy of the eggshell
 
 set -x
 
 ps -o ppid=
+whoami
 ./eggshell data1.dat data2.dat
