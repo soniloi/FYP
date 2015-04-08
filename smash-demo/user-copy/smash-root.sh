9625d235056b5d781a8fa6a01b9dc649c28f19ba
soniloi <niloinso@tcd.ie>
2015-04-08 14:41:47 +0100

    Removed unused scripts from demo

--- smash-demo/user-copy/smash-root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Smash root copy of the eggshell
-
-set -x
-
-whoami
-../root-copy/eggshell data1.dat data2.dat
--- smash-demo/user-copy/smash-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Smash user copy of the eggshell
-
-set -x
-
-ps -o ppid=
-whoami
-./eggshell data1.dat data2.dat
