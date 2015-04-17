fb471926c7e752853aa8da72d07743b5408df315
soniloi <niloinso@tcd.ie>
2015-04-17 19:10:46 +0100

    Added caller script; fixed a really bad seeding error

---
+++ overflow/benchmarking/caller.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+	echo "Usage: $0 <path-to-Debug+Asserts>"
+	exit 1
+fi
+
+daa=$1
+total_versions=16
+runs_per_technique=10
+readings_per_run=4
+seed_initial=17
+
+RANDOM=$seed_initial
+
+for i in $(seq 1 $total_versions); do
+	seed=$RANDOM
+	echo $seed
+	./Runner.py $daa $i $runs_per_technique $readings_per_run $seed > "logs/v$i.log" 2>&1 &
+done
+wait
