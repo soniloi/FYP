17c5a90f6a7727b62587c96512851a8d97fe2b2d
soniloi <niloinso@tcd.ie>
2015-04-20 15:05:15 +0100

    Commented out a load of completely useless stuff from parse_logs

--- evaluation/benchmarking/parse_logs.sh
+++ evaluation/benchmarking/parse_logs.sh
@@ -4,22 +4,6 @@ grepnc='grep --color=never'
 
 mkdir -p results
 
-if false; then
-$grepnc Single.*bb ./logs/*.log > results/single-bb.log
-$grepnc Single.*func ./logs/*.log > results/single-func.log
-$grepnc Single.*alloc\-insert\-4 ./logs/*.log > results/single-alloc4.log
-$grepnc Single.*alloc\-insert\-6 ./logs/*.log > results/single-alloc6.log
-
-$grepnc Aggregate.*bb ./logs/*.log > results/aggregate-bb.log
-$grepnc Aggregate.*func ./logs/*.log > results/aggregate-func.log
-$grepnc Aggregate.*alloc\-insert\-4 ./logs/*.log > results/aggregate-alloc4.log
-$grepnc Aggregate.*alloc\-insert\-6 ./logs/*.log > results/aggregate-alloc6.log
-
-$grepnc Single.*retired ./logs/*.log > results/single-retired.log
-$grepnc Single.*size ./logs/*.log > results/single-size.log
-$grepnc Single.*heap ./logs/*.log > results/single-heap.log
-fi
-
 $grepnc Aggregate.*retired ./logs/*.log > results/aggregate-retired.log
 $grepnc "\-alloc\-insert\-4" results/aggregate-retired.log > results/aggregate-retired-alloc4.log
 $grepnc "\-alloc\-insert\-6" results/aggregate-retired.log > results/aggregate-retired-alloc6.log
