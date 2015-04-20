d65fbe2561ed717bb1c3c286759e54fcddb66f5b
soniloi <niloinso@tcd.ie>
2015-04-20 15:06:03 +0100

    Got rid of all the commented-out stuff in heapcheck

--- evaluation/scripts/heapcheck.sh
+++ evaluation/scripts/heapcheck.sh
@@ -21,19 +21,6 @@ fi
 
 bindir=`dirname $bin`
 logfilebin="$bindir/heapcheck.log"
-#data1="$bindir/data1.dat"
-#data2="$bindir/data2.dat"
-#data1saved="$data1.saved"
-#data2saved="$data2.saved"
-
-# We need the datafiles not to be there; if the program overflows the stack, then valgrind will not provide a report
-#if [[ -f "$data2" ]]; then
-#	mv $data2 $data2saved
-#fi
-
-#if [[ -f "$data1" ]]; then
-#	mv $data1 $data1saved
-#fi
 
 com=`cat $input | $checker "--log-file=$logfilebin" $bin > /dev/null 2>&1`
 $com 2>/dev/null
@@ -43,14 +30,6 @@ heapuse=`echo -n ${heapwords[8]} | tr -d ,`
 
 # Clean up
 rm -f $logfilebin
-
-#if [[ -f "$data1saved" ]]; then
-#	mv $data1saved $data1
-#fi
-
-#if [[ -f "$data2saved" ]]; then
-#	mv $data2saved $data2
-#fi
 
 echo $heapuse
 
